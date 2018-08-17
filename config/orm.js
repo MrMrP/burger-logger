954a5aa8e1d1c30610c8a7dba1691768e71c6a3b
Marcus Price <marcusprice@MacBook-Pro.local>
2018-08-17 04:08:58 -0400

    finalizing directory structure

---
+++ config/orm.js
@@ -0,0 +1,11 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+function selectAll()
+
+function insertOne()
+
+function updateOne()
+
+
+module.exports = orm;
---
+++ controllers/burger_controllers.js
@@ -0,0 +1,12 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the ORM to create functions that will interact with the database.
+var burgers = require("../config/burgers.js");
+
+// Create all our routes and set up logic within those routes where required.
+
+
+
+module.exports = router;
